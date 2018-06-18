c3761dfc7d0a2e2eed7e8558914d968f752f45e1
g k <gkosmo1@hotmail.com>
2018-06-18 10:09:31 +0200

    beginning of my world cup website

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
   def home
+  end
+
+  def dashboard
+    @user_flats = current_user.flats
   end
 end
