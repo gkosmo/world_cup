0b23df61925939671479ce187d2144bc61789cf9
g k <gkosmo1@hotmail.com>
2018-06-18 10:22:31 +0200

    open json link to matches and showed them on index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
+require 'open-uri'
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
+    matches_url = 'http://worldcup.sfg.io/matches/today'
+    matches_json = open(matches_url).read
+    @matches = JSON.parse(matches_json)
   end
 
   def dashboard
