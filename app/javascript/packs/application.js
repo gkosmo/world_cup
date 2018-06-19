a5523c1dcac5ea8bcb2e0a5c5d91c32bbb63cf99
g k <gkosmo1@hotmail.com>
2018-06-19 15:11:28 +0200

    all

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,44 @@
 import "bootstrap";
 
+function startMe(){
+  console.log(Date.now());
+    fetch('http://worldcup.sfg.io/matches/current').then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+   let doc_c =  document.getElementById('current-match');
+   doc_c.innerHTML = '';
+  let mainDoc = document.createElement("div");
+  let p = myJson[0];
+
+  p = myJson[0]['home_team_events'][0];
+
+  let line = document.createElement('div');
+   line.innerText = "Home Team Events: ";
+ mainDoc.appendChild(line)
+for (var key in p) {
+  let line = document.createElement('div');
+    if (p.hasOwnProperty(key)) {
+        console.log( key + " -> " + p[key]);
+      line.innerText = key + " -> " + p[key];
+      mainDoc.appendChild(line);
+    }
+}
+
+ p = myJson[0]['away_team_events'][0];
+   line = document.createElement('div');
+   line.innerText = "Away Team Events: ";
+ mainDoc.appendChild(line)
+for (var key in p) {
+  let line = document.createElement('div');
+    if (p.hasOwnProperty(key)) {
+        console.log( key + " -> " + p[key]);
+      line.innerText = key + " -> " + p[key];
+      mainDoc.appendChild(line);
+    }
+}
+   doc_c.appendChild(mainDoc)
+  });
+}
+
+  setInterval(startMe, 1400);
