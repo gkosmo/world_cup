9a44eb85ab7d899d2aee1a689414382d21e0a2ba
g k <gkosmo1@hotmail.com>
2018-06-19 08:54:15 +0200

    added in the seeds and the main controller the seeds from the api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,8 @@ require 'open-uri'
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
+     Match.update_all_matches
      @matches =  Match.where(datetime: DateTime.now.all_day)
-  end
-
-  def dashboard
-    @user_flats = current_user.flats
+      
   end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,20 @@
+require 'open-uri'
 class Match < ApplicationRecord
+
+
+
+
+    def self.update_all_matches
+
+        matches_url = 'http://worldcup.sfg.io/matches'
+        matches_json = open(matches_url).read
+        matches_json = JSON.parse(matches_json)
+        matches_json.each do |mat|
+            match_db = Match.where(fifa_id: mat['fifa_id'] )[0]
+            match_db.update(datetime: mat["datetime"], home_team: mat["home_team"]["country"], away_team: mat["away_team"]["country"], venue: mat["venue"] ) 
+        end
+    end
+
+
+
 end
