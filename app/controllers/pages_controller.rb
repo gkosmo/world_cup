36f87b7d7042f0cc4fa176cba1329fdc0e225cd4
g k <gkosmo1@hotmail.com>
2018-06-20 10:43:25 +0200

    added search by venue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,4 +17,20 @@ class PagesController < ApplicationController
       Vote.create(user: current_user, team: params[:team], match_id: params[:match])
       redirect_to root_path
   end
+
+
+  def search
+    @matches =  Match.near(query_search[:query], 200)
+     matches_with_coord = @matches.where.not(latitude: nil)
+     @markers  = matches_with_coord.map do |match|
+        {
+          lng: match.longitude,
+          lat: match.latitude
+          }
+     end
+  end
+
+  def query_search
+    params.require(:query).permit(:query)
+  end
 end
